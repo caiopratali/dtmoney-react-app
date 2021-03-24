cec237b0279bad15e8567a8795ec9d826b356a7d
Caio Pratali <caio@pratali.com.br>
2021-03-24 13:34:41 -0300

    Create GlobalStyled

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { GlobalStyled } from "./styles/global";
+
 export function App() {
-  return <h1>Hi Dev!</h1>
+  return (
+    <>
+      <h1>Hi Dev!</h1>
+      <GlobalStyled />
+    </>
+  );
 }
